03b5f74c20843e314559e0bf55a0eedf81b688f6
Yushen Sun <sun.yushen@gmail.com>
2023-03-13 17:19:21 +0000

    Use 5-fold cross-validation to find out the best depth of the decision tree. Current result is 7

--- decision_tree.py
+++ decision_tree.py
@@ -1,21 +1,26 @@
 import pandas as pd
+import numpy as np
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import accuracy_score
+from sklearn.tree import export_graphviz
+from graphviz import Source
 
 # Load the data from the CSV file
-data_train = pd.read_csv('data_train.csv')
+data_train_val = pd.read_csv('data_train.csv')
 data_test = pd.read_csv('data_test.csv')
 
 # Convert a categorical variable to numerical values
 encoder = LabelEncoder()
-data_train['workclass'] = encoder.fit_transform(data_train['workclass'])
-data_train['marital-status'] = encoder.fit_transform(data_train['marital-status'])
-data_train['occupation'] = encoder.fit_transform(data_train['occupation'])
-data_train['relationship'] = encoder.fit_transform(data_train['relationship'])
-data_train['race'] = encoder.fit_transform(data_train['race'])
-data_train['gender'] = encoder.fit_transform(data_train['gender'])
-data_train['native-country'] = encoder.fit_transform(data_train['native-country'])
+data_train_val['workclass'] = encoder.fit_transform(data_train_val['workclass'])
+data_train_val['marital-status'] = encoder.fit_transform(data_train_val['marital-status'])
+data_train_val['occupation'] = encoder.fit_transform(data_train_val['occupation'])
+data_train_val['relationship'] = encoder.fit_transform(data_train_val['relationship'])
+data_train_val['race'] = encoder.fit_transform(data_train_val['race'])
+data_train_val['gender'] = encoder.fit_transform(data_train_val['gender'])
+data_train_val['native-country'] = encoder.fit_transform(data_train_val['native-country'])
 data_test['workclass'] = encoder.fit_transform(data_test['workclass'])
 data_test['marital-status'] = encoder.fit_transform(data_test['marital-status'])
 data_test['occupation'] = encoder.fit_transform(data_test['occupation'])
@@ -24,13 +29,41 @@ data_test['race'] = encoder.fit_transform(data_test['race'])
 data_test['gender'] = encoder.fit_transform(data_test['gender'])
 data_test['native-country'] = encoder.fit_transform(data_test['native-country'])
 
-# Set x_train and X_test to contain columns col1 and col2 of the DataFrame
-x_train = data_train[
-    ['age', 'workclass', 'fnlwgt', 'educational_num', 'marital-status', 'occupation', 'relationship', 'race', 'gender',
-     'capital-gain', 'capital-loss', 'hours-per-week', 'native-country']]
-x_test = data_test[
-    ['age', 'workclass', 'fnlwgt', 'educational_num', 'marital-status', 'occupation', 'relationship', 'race', 'gender',
-     'capital-gain', 'capital-loss', 'hours-per-week', 'native-country']]
+# Set x_train and x_test to contain columns of the DataFrame
+feature_names = ['age', 'workclass', 'fnlwgt', 'educational_num', 'marital-status', 'occupation', 'relationship',
+                 'race', 'gender', 'capital-gain', 'capital-loss', 'hours-per-week', 'native-country']
+
+# Split training data into real training and validation sets
+X_train, X_val, y_train, y_val = train_test_split(data_train_val[feature_names], data_train_val['income'], test_size=0.2, random_state=42)
+
+# Define range of values for maximum depth of tree
+max_depth_values = range(1, len(feature_names))
+
+# Loop over values of max_depth and calculate cross-validation score for each
+cv_scores = []
+for max_depth in max_depth_values:
+    clf = DecisionTreeClassifier(max_depth=max_depth)
+    scores = cross_val_score(clf, X_train, y_train, cv=5)
+    cv_scores.append(scores.mean())
+
+# Choose value of max_depth that gives best cross-validation score
+best_max_depth = max_depth_values[np.argmax(cv_scores)]
+
+print(best_max_depth)
+
+# Train final model using best value of max_depth
+final_clf = DecisionTreeClassifier(max_depth=best_max_depth)
+final_clf.fit(X_train, y_train)
+
+# Evaluate performance on validation set
+val_score = final_clf.score(X_val, y_val)
+
+
+'''
+
+
+x_train = data_train[feature_names]
+x_test = data_test[feature_names]
 
 # Set y_train and y_test to contain the target variable of the DataFrame
 y_train = data_train['income']
@@ -48,3 +81,22 @@ y_pred = clf.predict(x_test)
 # Evaluate the accuracy of the classifier
 accuracy = accuracy_score(y_test, y_pred)
 print(f"Accuracy: {accuracy}")
+
+# Visualize the decision tree
+export_graphviz(clf, out_file="tree.dot", feature_names=feature_names, filled=True,
+                rounded=True, special_characters=True)
+
+with open("tree.dot") as f:
+    dot_graph = f.read()
+
+graph = Source(dot_graph)
+graph.format = "png"
+graph.render("decision_tree", view=True)
+# dot: graph is too large for cairo-renderer bitmaps. Scaling by 0.194084 to fit
+
+
+'''
+
+
+
+
